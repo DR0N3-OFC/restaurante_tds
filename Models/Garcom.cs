27a00bf5de955f9586c08c7a73febac0aeb1a2bb
AugustoSebenDaRosa <52408519+snykral@users.noreply.github.com>
2023-04-10 08:51:34 -0300

    c2

--- Models/Garcom.cs
+++ Models/Garcom.cs
@@ -3,23 +3,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Aula03.Models
 {
-    public class Atendimento
+    public class Garcom
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 
+
         public int? ID { get; set; }
 
-        [Required(ErrorMessage = "Garçom é obrigatório")]
-        public Garcom? Garcom { get; set; }
 
-        [Required(ErrorMessage = "Mesa é obrigatória")]
-        public Mesa? Mesa { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string? Name { get; set; }
+
+
+        [Required(ErrorMessage = "Gênero é obrigatório")]
+        public string? Gender { get; set; }
+
 
         [Required(ErrorMessage = "Data é obrigatória")]
         [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
-        public DateTime? InitDate { get; set; }
-        public DateTime? FinishDate { get; set; }
-        public bool? Status { get; set; }
+        public DateTime? BirthDate { get; set; }
     }
 }
---
+++ Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aula03.Models
+{
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+
+        public int? ID { get; set; }
+
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Preço é obrigatório")]
+        public double? Price { get; set; }
+    }
+}
